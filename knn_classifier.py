af813c75f7483e40dcb4942a67cda01ff5439c68
Splugen <jgurakuqi96@gmail.com>
2022-04-08 21:48:50 +0200

    Updated SVM: added tests for different Cs

--- knn_classifier.py
+++ knn_classifier.py
@@ -3,36 +3,10 @@ from sklearn.model_selection import cross_val_score
 import numpy as np
 
 
-def printOutput(result):
-    print(
-        "=========================================================="
-        + "\n"
-        + "K-NN classification with K = 5"
-        + "\n"
-        + "Minimum Accuracy Classifier: "
-        + str(result.min())
-        + "\n"
-        + "Average Accuracy Classifier: "
-        + str(result.mean())
-        + "\n"
-        + "Maximum Accuracy Classifier: "
-        + str(result.max())
-        + "\n"
-        + "Variance of Accuracy/Standard Deviation of Accuracy: "
-        + str(result.var())
-        + " / "
-        + str(result.std())
-        + "\n"
-        + "=========================================================="
-    )
-
-
 def k_nn(mailData):
-    # np.random.shuffle(mailData)
     X = mailData[:, :54]  # values
     y = mailData[:, 57]  # classes
-
-    k_nn_classifier = KNeighborsClassifier(n_neighbors=5, p=2, metric="euclidean")
-    k_nn_scores = cross_val_score(k_nn_classifier, X, y, cv=10)
-    # printOutput(k_nn_scores)
+    k_nn_scores = cross_val_score(
+        KNeighborsClassifier(n_neighbors=5, p=2, metric="euclidean"), X, y, cv=10
+    )
     return k_nn_scores
--- svm_classifier.py
+++ svm_classifier.py
@@ -2,6 +2,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 import numpy as np
+from time import time
 
 
 # This function returns the weight of the term in the document.
@@ -60,21 +61,16 @@ def svm(mailData):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     # SVM with linear kernel
-
     linear_classifier = SVC(kernel="linear", C=1.0)
     scores_linear = cross_val_score(linear_classifier, X, y, cv=10)
     linear_classifier_fit = linear_classifier.fit(X_train, y_train)
-    # printOutput(scores_linear, "LINEAR", linear_classifier_fit.n_support_)
 
     # SVM with polynomial of degree 2 kernel
-
     poly2_classifier = SVC(kernel="poly", degree=2, C=1.0)
     scores_poly2 = cross_val_score(poly2_classifier, X, y, cv=10)
     poly2_classifier_fit = poly2_classifier.fit(X_train, y_train)
-    # printOutput(scores_poly2, "POLYNOMIAL OF DEGREE 2", poly2_classifier_fit.n_support_)
 
     # SVM with RBF kernel
-
     radial_basis_function_classifier = SVC(kernel="rbf", C=1.0)
     scores_radial_basis_function = cross_val_score(
         radial_basis_function_classifier, X, y, cv=10
@@ -82,8 +78,59 @@ def svm(mailData):
     radial_basis_function_clf_fit = radial_basis_function_classifier.fit(
         X_train, y_train
     )
-    # printOutput( scores_radial_basis_function, "RBF", radial_basis_function_clf_fit.n_support_)
     return scores_linear, scores_poly2, scores_radial_basis_function
+
+
+def svm_with_different_Cs(mailData):
+    X, y = preprocessing_data(mailData)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    Cs = [1, 25, 50, 75, 100]
+    linear_results = []
+    poly2_results = []
+    rbf_results = []
+    for C in Cs:
+        # Classifier model init and Cross validation
+        start = time()
+        linear_classifier = SVC(kernel="linear", C=C)
+        linear_score = cross_val_score(linear_classifier, X, y, cv=10)
+        linear_end = time()
+        poly2_classifier = SVC(kernel="poly", degree=2, C=C)
+        poly2_score = cross_val_score(poly2_classifier, X, y, cv=10)
+        poly2_end = time()
+        radial_basis_function_classifier = SVC(kernel="rbf", C=C)
+        rbf_score = cross_val_score(radial_basis_function_classifier, X, y, cv=10)
+        rbf_end = time()
+
+        # Storing results
+        linear_results.append(
+            [
+                linear_score,
+                linear_classifier.fit(X_train, y_train).n_support_,
+                linear_end - start,
+                "Linear SVC",
+                C,
+            ]
+        )
+        poly2_results.append(
+            [
+                poly2_score,
+                poly2_classifier.fit(X_train, y_train).n_support_,
+                poly2_end - start,
+                "2-degree Poly SVC",
+                C,
+            ]
+        )
+        rbf_results.append(
+            [
+                rbf_score,
+                radial_basis_function_classifier.fit(X_train, y_train).n_support_,
+                rbf_end - start,
+                "RBF SVC",
+                C,
+            ]
+        )
+
+    return linear_results, poly2_results, rbf_results
 
 
 ###################################################################
@@ -104,50 +151,90 @@ def svm_angular(mailData):
     ) = train_test_split(X_norm, y, test_size=0.3)
 
     # SVM with linear kernel
-    linear_classifier_norm = SVC(kernel="linear", C=1.0)
-    scores_linear_norm = cross_val_score(linear_classifier_norm, X_norm, y, cv=10)
-    linear_classifier_norm_fit = linear_classifier_norm.fit(
+    norm_linear_classifier = SVC(kernel="linear", C=1.0)
+    norm_linear_score = cross_val_score(norm_linear_classifier, X_norm, y, cv=10)
+    norm_linear_classifier_fit = norm_linear_classifier.fit(
         tfidf(X_norm_train), y_norm_train
     )
-    # printOutput(scores_linear_norm, "LINEAR", linear_classifier_norm_fit.n_support_)
-
     # SVM with polynomial of degree 2 kernel
-
-    poly2_classifier_norm = SVC(kernel="poly", degree=2, C=1.0)
-    scores_poly2_norm = cross_val_score(poly2_classifier_norm, X_norm, y, cv=10)
-    poly2_classifier_norm_fit = poly2_classifier_norm.fit(
+    norm_poly2_classifier = SVC(kernel="poly", degree=2, C=1.0)
+    norm_poly2_score = cross_val_score(norm_poly2_classifier, X_norm, y, cv=10)
+    norm_poly2_classifier_fit = norm_poly2_classifier.fit(
         tfidf(X_norm_train), y_norm_train
     )
-    # printOutput(
-    #     scores_poly2_norm,
-    #     "POLYINOMIAL OF DEGREE 2",
-    #     poly2_classifier_norm_fit.n_support_,
-    # )
-
     # SVM with RBF kernel
-
-    radial_basis_function_clf_norm = SVC(kernel="rbf", C=1.0)
-    scores_radial_basis_function_norm = cross_val_score(
-        radial_basis_function_clf_norm, X_norm, y, cv=10
-    )
-    radial_basis_fun_clf_norm_fit = radial_basis_function_clf_norm.fit(
+    norm_rbf_classifier = SVC(kernel="rbf", C=1.0)
+    norm_rbf_score = cross_val_score(norm_rbf_classifier, X_norm, y, cv=10)
+    radial_basis_fun_clf_norm_fit = norm_rbf_classifier.fit(
         tfidf(X_norm_train), y_norm_train
     )
-    # printOutput(
-    #     scores_radial_basis_function_norm,
-    #     "RBF",
-    #     radial_basis_fun_clf_norm_fit.n_support_,
-    # )
-    return scores_linear_norm, scores_poly2_norm, scores_radial_basis_function_norm
+    return norm_linear_score, norm_poly2_score, norm_rbf_score
+
+
+def angular_svm_with_different_Cs(mailData):
+    X, y = preprocessing_data(mailData)
+    # TODO: add comment about the following 2 lines.
+    norms = np.sqrt(((X + 1e-100) ** 2).sum(axis=1, keepdims=True))
+    X_norm = np.where(norms > 0.0, X / norms, 0.0)
+    (
+        X_norm_train,
+        X_norm_test,
+        y_norm_train,
+        y_norm_test,
+    ) = train_test_split(X_norm, y, test_size=0.3)
+    Cs = [1, 25, 50, 75, 100]
+    norm_linear_results = []
+    norm_poly2_results = []
+    norm_rbf_results = []
+    for C in Cs:
+        # Classifier model init and Cross validation
+        start = time()
+        norm_linear_classifier = SVC(kernel="linear", C=C)
+        norm_linear_score = cross_val_score(norm_linear_classifier, X_norm, y, cv=10)
+        linear_end = time()
+        norm_poly2_classifier = SVC(kernel="poly", degree=2, C=C)
+        norm_poly2_score = cross_val_score(norm_poly2_classifier, X_norm, y, cv=10)
+        poly2_end = time()
+        norm_rbf_classifier = SVC(kernel="rbf", C=C)
+        norm_rbf_score = cross_val_score(norm_rbf_classifier, X_norm, y, cv=10)
+        rbf_end = time()
+
+        # Storing results
+        norm_linear_results.append(
+            [
+                norm_linear_score,
+                norm_linear_classifier.fit(
+                    tfidf(X_norm_train), y_norm_train
+                ).n_support_,
+                linear_end - start,
+                "Norm Linear SVC",
+                C,
+            ]
+        )
+        norm_poly2_results.append(
+            [
+                norm_poly2_score,
+                norm_poly2_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
+                poly2_end - start,
+                "Norm 2-degree Poly SVC",
+                C,
+            ]
+        )
+        norm_rbf_results.append(
+            [
+                norm_rbf_score,
+                norm_rbf_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
+                rbf_end - start,
+                "Norm RBF Poly SVC",
+                C,
+            ]
+        )
+
+    return norm_linear_results, norm_poly2_results, norm_rbf_results
 
 
 # TODO:
-# - cerca di capire se va usato solo n_support_ o anche altri parametri
-#    - Buoso utilizza anche questo che ti fa un report e fa due stampe in piu:
-#         metrics.classification_report(y_true=y_test, y_pred = y_pred)
-#         print('Mean fit time =', np.mean(res['fit_time']))
-#         print('Mean score time =', np.mean(res['score_time']))
-#
+
 # - cerca di capire che altre cose servono/mancano rispetto agli altri e se sono cose
 #   utili o meno, tipo grafici o altri confronti
 #     - Buoso mette questo:

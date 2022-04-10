bae0843f19dd208f610cb6eaa1506a3e11903c41
Splugen <jgurakuqi96@gmail.com>
2022-04-10 18:15:39 +0200

    Removed unnecessary comments and functions

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,12 +1,25 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
+import cpuinfo
 import numpy as np
 
 
+if "Intel" in cpuinfo.get_cpu_info()["brand_raw"]:
+    from sklearnex import patch_sklearn
+
+    patch_sklearn()
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.model_selection import cross_val_score
+
+
 def k_nn(mailData):
-    X = mailData[:, :54]  # values
-    y = mailData[:, 57]  # classes
+    np.random.shuffle(mailData)
+    # X = mailData[:, :54]  # values
+    # y = mailData[:, 57]  # classes
     k_nn_scores = cross_val_score(
-        KNeighborsClassifier(n_neighbors=5, p=2, metric="euclidean"), X, y, cv=10
+        KNeighborsClassifier(n_neighbors=5, p=2, metric="euclidean"),
+        mailData[:, :54],
+        mailData[:, 57],
+        cv=10,
     )
     return k_nn_scores
--- svm_classifier.py
+++ svm_classifier.py
@@ -1,9 +1,19 @@
+import cpuinfo
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 import numpy as np
 from time import time
 
+
+if "Intel" in cpuinfo.get_cpu_info()["brand_raw"]:
+    from sklearnex import patch_sklearn
+
+    patch_sklearn()
+
+    from sklearn.svm import SVC
+    from sklearn.model_selection import train_test_split
+    from sklearn.model_selection import cross_val_score
 
 # This function returns the weight of the term in the document.
 def tfidf(mailData):
@@ -13,72 +23,17 @@ def tfidf(mailData):
 
 
 def preprocessing_data(mailData):
-    # np.random.shuffle(mailData)
+    np.random.shuffle(mailData)
     X = mailData[:, :54]  # values
     y = mailData[:, 57]  # classes
     X = tfidf(X)
-
     return X, y
-
-
-# def printOutput(result, name_method, num_vectors):
-#     print(
-#         "=========================================================="
-#         + "\n"
-#         + "SVM classification using "
-#         + name_method
-#         + " kernel:"
-#         + "\n"
-#         + "Minimum Accuracy Kernel: "
-#         + str(result.min())
-#         + "\n"
-#         + "Average Accuracy Kernel: "
-#         + str(result.mean())
-#         + "\n"
-#         + "Maximum Accuracy Kernel: "
-#         + str(result.max())
-#         + "\n"
-#         + "Variance of Accuracy/Standard Deviation of Accuracy: "
-#         + str(result.var())
-#         + " / "
-#         + str(result.std())
-#         + "\n"
-#         + "Number of support vectors used for a trained SVM: "
-#         + str(num_vectors)
-#         + "\n"
-#         + "=========================================================="
-#     )
 
 
 ###################################################################
 # SVM classification using linear, polynomial of degree 2 and RBF #
 #  kernels over the TF/IDF representation. #
 ###################################################################
-
-
-def svm(mailData):
-    X, y = preprocessing_data(mailData)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-    # SVM with linear kernel
-    linear_classifier = SVC(kernel="linear", C=1.0)
-    scores_linear = cross_val_score(linear_classifier, X, y, cv=10)
-    linear_classifier_fit = linear_classifier.fit(X_train, y_train)
-
-    # SVM with polynomial of degree 2 kernel
-    poly2_classifier = SVC(kernel="poly", degree=2, C=1.0)
-    scores_poly2 = cross_val_score(poly2_classifier, X, y, cv=10)
-    poly2_classifier_fit = poly2_classifier.fit(X_train, y_train)
-
-    # SVM with RBF kernel
-    radial_basis_function_classifier = SVC(kernel="rbf", C=1.0)
-    scores_radial_basis_function = cross_val_score(
-        radial_basis_function_classifier, X, y, cv=10
-    )
-    radial_basis_function_clf_fit = radial_basis_function_classifier.fit(
-        X_train, y_train
-    )
-    return scores_linear, scores_poly2, scores_radial_basis_function
 
 
 def svm_with_different_Cs(mailData):
@@ -100,33 +55,56 @@ def svm_with_different_Cs(mailData):
         rbf_end = time()
 
         # Storing results
-        results.append(
+        results += [
             [
                 linear_score,
                 linear_classifier.fit(X_train, y_train).n_support_,
                 linear_end - start,
                 "Linear SVC",
                 C,
-            ]
-        )
-        results.append(
+            ],
             [
                 poly2_score,
                 poly2_classifier.fit(X_train, y_train).n_support_,
                 poly2_end - start,
                 "2-degree Poly SVC",
                 C,
-            ]
-        )
-        results.append(
+            ],
             [
                 rbf_score,
                 radial_basis_function_classifier.fit(X_train, y_train).n_support_,
                 rbf_end - start,
                 "RBF SVC",
                 C,
-            ]
-        )
+            ],
+        ]
+        # results.append(
+        #     [
+        #         linear_score,
+        #         linear_classifier.fit(X_train, y_train).n_support_,
+        #         linear_end - start,
+        #         "Linear SVC",
+        #         C,
+        #     ]
+        # )
+        # results.append(
+        #     [
+        #         poly2_score,
+        #         poly2_classifier.fit(X_train, y_train).n_support_,
+        #         poly2_end - start,
+        #         "2-degree Poly SVC",
+        #         C,
+        #     ]
+        # )
+        # results.append(
+        #     [
+        #         rbf_score,
+        #         radial_basis_function_classifier.fit(X_train, y_train).n_support_,
+        #         rbf_end - start,
+        #         "RBF SVC",
+        #         C,
+        #     ]
+        # )
 
     return results
 
@@ -135,43 +113,10 @@ def svm_with_different_Cs(mailData):
 # SVM classification using linear, polynomial of degree 2 and RBF #
 #                  kernels with ANGULAR INFORMATION.              #
 ###################################################################
-def svm_angular(mailData):
-
-    X, y = preprocessing_data(mailData)
-    # TODO: add comment about the following 2 lines.
-    norms = np.sqrt(((X + 1e-100) ** 2).sum(axis=1, keepdims=True))
-    X_norm = np.where(norms > 0.0, X / norms, 0.0)
-    (
-        X_norm_train,
-        X_norm_test,
-        y_norm_train,
-        y_norm_test,
-    ) = train_test_split(X_norm, y, test_size=0.3)
-
-    # SVM with linear kernel
-    norm_linear_classifier = SVC(kernel="linear", C=1.0)
-    norm_linear_score = cross_val_score(norm_linear_classifier, X_norm, y, cv=10)
-    norm_linear_classifier_fit = norm_linear_classifier.fit(
-        tfidf(X_norm_train), y_norm_train
-    )
-    # SVM with polynomial of degree 2 kernel
-    norm_poly2_classifier = SVC(kernel="poly", degree=2, C=1.0)
-    norm_poly2_score = cross_val_score(norm_poly2_classifier, X_norm, y, cv=10)
-    norm_poly2_classifier_fit = norm_poly2_classifier.fit(
-        tfidf(X_norm_train), y_norm_train
-    )
-    # SVM with RBF kernel
-    norm_rbf_classifier = SVC(kernel="rbf", C=1.0)
-    norm_rbf_score = cross_val_score(norm_rbf_classifier, X_norm, y, cv=10)
-    radial_basis_fun_clf_norm_fit = norm_rbf_classifier.fit(
-        tfidf(X_norm_train), y_norm_train
-    )
-    return norm_linear_score, norm_poly2_score, norm_rbf_score
 
 
 def angular_svm_with_different_Cs(mailData):
     X, y = preprocessing_data(mailData)
-    # TODO: add comment about the following 2 lines.
     norms = np.sqrt(((X + 1e-100) ** 2).sum(axis=1, keepdims=True))
     X_norm = np.where(norms > 0.0, X / norms, 0.0)
     (
@@ -196,7 +141,7 @@ def angular_svm_with_different_Cs(mailData):
         rbf_end = time()
 
         # Storing results
-        results.append(
+        results += [
             [
                 norm_linear_score,
                 norm_linear_classifier.fit(
@@ -205,40 +150,50 @@ def angular_svm_with_different_Cs(mailData):
                 linear_end - start,
                 "Norm Linear SVC",
                 C,
-            ]
-        )
-        results.append(
+            ],
             [
                 norm_poly2_score,
                 norm_poly2_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
                 poly2_end - start,
                 "Norm 2-degree Poly SVC",
                 C,
-            ]
-        )
-        results.append(
+            ],
             [
                 norm_rbf_score,
                 norm_rbf_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
                 rbf_end - start,
                 "Norm RBF SVC",
                 C,
-            ]
-        )
+            ],
+        ]
+        # results.append(
+        #     [
+        #         norm_linear_score,
+        #         norm_linear_classifier.fit(
+        #             tfidf(X_norm_train), y_norm_train
+        #         ).n_support_,
+        #         linear_end - start,
+        #         "Norm Linear SVC",
+        #         C,
+        #     ]
+        # )
+        # results.append(
+        #     [
+        #         norm_poly2_score,
+        #         norm_poly2_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
+        #         poly2_end - start,
+        #         "Norm 2-degree Poly SVC",
+        #         C,
+        #     ]
+        # )
+        # results.append(
+        #     [
+        #         norm_rbf_score,
+        #         norm_rbf_classifier.fit(tfidf(X_norm_train), y_norm_train).n_support_,
+        #         rbf_end - start,
+        #         "Norm RBF SVC",
+        #         C,
+        #     ]
+        # )
 
     return results
-
-
-# TODO:
-
-# - cerca di capire che altre cose servono/mancano rispetto agli altri e se sono cose
-#   utili o meno, tipo grafici o altri confronti
-#     - Buoso mette questo:
-#       # SVM with an higher C parameter to make the classifier more stringent in
-#       the classification of outliers
-#       clf = svm.SVC(kernel = "linear", C = 100)
-#
-# - cerca di capire i n_jobs utili e necessari
-#     da verificare durante le stampe e i test.
-#
-# - migliora i vari commenti in giro
